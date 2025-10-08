89e69353c00bc9738e5c0be97eb6d713ed0380c6
aritra biswas <aritrabiswas0085@gmail.com>
2025-10-08 19:51:04 +0530

    import the sidebar in layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Sidebar from './Sidebar'
 
 const Layout = ({onLogout,user}) => {
   return (
     <div className='min-h-screen bg-gray-50'>
       <Navbar user={user} onLogout={onLogout}/>
+      <Sidebar user={user} tasks={tasks}/>
     </div>
   )
 }
