712fc0d16bdde8251095939c5f0688e273e485a2
aritra biswas <aritrabiswas0085@gmail.com>
2025-10-08 15:58:01 +0530

    working on navbar and logout and settings page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import {Settings, Zap} from 'lucide-react'
+import {ChevronDown, LogOut, Settings, Zap} from 'lucide-react'
 
-const Navbar = () => {
-
+const Navbar = (user={}, onLogout) => {
+  const menuref = useRef(null);
+  const [menuOpen, setMenuOpen] = useState(false);
   const navigate = useNavigate();
+
+  const handleMenuToggle = () => setMenuOpen((prev) => !prev);
+  const handleLogout = () => {
+    setMenuOpen(false)
+    onLogout()
+  }  
   return (
     <header className='sticky top-0 z-50 bg-white/90 backdrop-blur-md shadow-sm border-d border-gray-200 font-sans'>
       <div className='flex items-center justify-between px-4 py-3 md:px-6 max-w-7xl mx-auto'>
@@ -31,6 +38,49 @@ const Navbar = () => {
             </button>
 
             {/* USER DROPDOWN */}
+            <div ref={menuref} className='relative'>
+                <button onClick={handleMenuToggle} className='flex items-center gap-2 px-3 py-2 rounded-full cursor-pointer hover:bg-purple-50 transition-colors duration-300 border border-transparent hover:border-purple-200'>
+                  <div className='relative'>
+                    {user.avatar ? (
+                      <img src={user.avatar} alt="Avatar" className='w-9 h-9 rounded-full shadow-sm'/>
+                    ) : (
+                      <div className='w-8 h-8 rounded-full  flex items-center justify-center bg-gradient-to-br from-fuchsia-500 to-purple-600 text-white font-semibold shadow-md'>
+                        {user.name?.[0]?.toUpperCase() || 'U'}
+                      </div>
+                    )}
+                    <div className='absolute -bottom-0.5 -right-0.5 w-3 h-3 bg-green-400 rounded-full border-2 border-white animate-pulse'/>
+                  </div>
+
+                  <div className='text-left hidden md:block'>
+                    <p className='text-sm font-medium text-gray-800'>{user.name}</p>
+                    <p className='text-xs text-gray-500 font-normal'>{user.email}</p>
+                  </div>
+
+                  <ChevronDown className={`w-4 h-4 text-gray-500 transition-transform duration-300 ${menuOpen ? 'rotate-180' : ''}`}/> 
+                </button>
+
+                {menuOpen && (
+                  <ul className='absolute top-14 right-0 w-56 bg-white rounded-2xl shadow-xl border border-purple-100 z-50 overflow-hidden animate-fadeIn'>
+                       <li className='p-2'>
+                          <button onClick={() => {
+                            setMenuOpen(false)
+                            navigate('/profile')
+                          }}
+                               className='w-full text-left px-4 py-2.5 text-gray-700 hover:bg-purple-50 text-sm transition-colors flex items-center gap-2 group' role='menuitem'>
+                            <Settings className='w-4 h-4 text-gray-700 group-hover:text-purple-500'/>
+                            Profile Settings
+                          </button>
+                       </li>
+
+                       <li className='p-2'>
+                          <button onClick={handleLogout} className='flex w-full items-center gap-2 rounded-lg px-3 py-2 text-sm hover:bg-red-50 text-red-600'>
+                              <LogOut className='w-4 h-4'/>
+                              Logout
+                          </button>
+                       </li>
+                  </ul>
+                )}
+            </div>
           </div>
       </div>
     </header>
