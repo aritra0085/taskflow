21fa667afe588294ad9dc30f7c9111a96e63b37c
aritra biswas <aritrabiswas0085@gmail.com>
2025-10-09 16:28:55 +0530

    import the tradingUp

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -3,6 +3,7 @@ import Navbar from './Navbar'
 import Sidebar from './Sidebar'
 import { Outlet } from 'react-router-dom';
 import axios from 'axios';
+import {TreadingUp} from 'lucide-react'
 
 const Layout = ({onLogout,user}) => {
 
