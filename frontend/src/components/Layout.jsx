4f147ee742ed8af4df7601b502adec16001c61c0
aritra biswas <aritrabiswas0085@gmail.com>
2025-10-09 16:09:29 +0530

    doen with loading section

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -75,6 +75,13 @@ const StatCard = ({title, value, icon}) => (
 )
 
   //Loading
+  if (loading) return (
+    <div className='min-h-screen bg-gray-50 flex items-center justify-center'>
+        <div className='animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-purple-500'>
+
+        </div>
+    </div>
+  )
 
   return (
     <div className='min-h-screen bg-gray-50'>
