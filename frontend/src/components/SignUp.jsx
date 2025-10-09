72962de75ecb187ac4f67d122a45e50d0e187b65
aritra biswas <aritrabiswas0085@gmail.com>
2025-10-10 00:04:15 +0530

    done with icon 56

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -54,6 +54,9 @@ const SignUp = () => {
           {FIELDS.map(({name, type, placeholder, icon: Icon}) => (
             <div key={name} className={Inputwrapper}>
                 <Icon className='text-purple-500 w-5 h-5 mr-2'/>
+
+                <input type={type} placeholder={placeholder} value={formData[name]} onChange={(e) =>setFormData({...formData,[name]:e.target.value})}
+                className='w-full focus:outline-none text-sm text-gray-700' required/>
             </div>
           ))}
       </from>
