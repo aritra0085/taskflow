f8238698831dc95fc2c781c812b5f800b4f699b4
aritra biswas <aritrabiswas0085@gmail.com>
2025-10-09 21:07:13 +0530

    continue with signUp.jsx

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import { Icon, UserPlus } from 'lucide-react'
+import React, { useState } from 'react'
+import {Inputwrapper, MESSAGE_ERROR, MESSAGE_SUCCESS} from '../assets/dummy'
+
+const API_URL = "http://localhost:4000/"
+const INITIAL_FORM = {name: "",email: "", password: ""}
 
 const SignUp = () => {
+
+  const [formData,setFormData] = useState(INITIAL_FORM);  
+  const [loading,setLoading] = useState(false)
+  const [message, setMessage] = useState({text: "", type:""})
+
+  
+
   return (
-    <div>
-      
+    <div className='max-w-md w-full bg-white shadow-lg border border-purple-100 rounded-xl p-8'>
+      <div className='mb-6 text-center'>
+        <div className='w-16 h-16 bg-gradient-to-br from-fuchsia-500 to-purple-600 rounded-full mx-auto flex items-center justify-center mb-4'>
+            <UserPlus className='w-8 h-8  text-white'/>
+        </div>
+        <h2 className='text-2xl font-bold text-gray-800'>
+            Create Account
+        </h2>
+        <p className='text-gray-500 text-sm mt-1'>Join TaskFlow to manage your tasks</p>
+      </div>
+
+      {message.text && (
+        <div className={message.type === 'success' ? MESSAGE_SUCCESS : MESSAGE_ERROR}>
+            {message.text}
+        </div>
+      )}  
+
+      <from onSubmit={handleSUbmit} className='space-y-4'>
+          {FIELDS.map(({name, type, placeholder, icon: Icon}) => (
+            <div key={name} className={Inputwrapper}>
+                <Icon className='text-purple-500 w-5 h-5 mr-2'/>
+            </div>
+          ))}
+      </from>
     </div>
+
+
   )
 }
 
