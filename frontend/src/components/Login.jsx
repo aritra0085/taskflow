89cc4f671c3e8caf4a015b12ed8a0e30494d62d1
aritra biswas <aritrabiswas0085@gmail.com>
2025-10-10 15:41:03 +0530

    starting with login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import {toast, ToastContainer} from 'react-toastify'
 const Login = () => {
   return (
-    <div>
+    <div className='max-w-md bg-white w-full shadow-lg border border-purple-100 rounded-xl p-8'>
+      <ToastContainer position='top center' autoClose={3000} hideProgressBar/>
       
     </div>
   )
