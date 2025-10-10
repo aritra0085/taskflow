4350531ef106687d5c82675b33d576fa2bb44b62
aritra biswas <aritrabiswas0085@gmail.com>
2025-10-10 18:55:06 +0530

    working with login button

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {toast, ToastContainer} from 'react-toastify'
+import { INPUTWRAPPER,fields } from '../assets/dummy'
+import { Eye, EyeOff, LogIn } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
+
+const INITIAL_FORM = {email: "", password: ""}
 const Login = () => {
+
+  const [showPassword, setShowPassword] = useState(false)
+  const [loading,setLoading] = useState(false)
+  const [formData, setFormData] = useState(INITIAL_FORM)
+  const [rememberMe,setRememberMe] = useState(false)
+  const navigate = useNavigate()
+  const url = 'http://localhost:4000/'
+
   return (
     <div className='max-w-md bg-white w-full shadow-lg border border-purple-100 rounded-xl p-8'>
       <ToastContainer position='top center' autoClose={3000} hideProgressBar/>
-      
+
+      <div className='mb-6 text-center'>
+          <div className='w-16 h-16 bg-gradient-to-br from-fuchsia-500 to-purple-600 rounded-full mx-auto flex items-center justify-center mb-4'>
+              <LogIn className='w-8 h-8 text-white'/>
+          </div>
+          <h2 className='text-2xl font-bold text-gray-800'>Welcome Back</h2>
+          <p className='text-gray-500 text-sm mt-1'>Sign in to continue to TaskFlow</p>
+      </div>
+
+      <form onSubmit={handleSubmit} className='space-y-4'>
+        {fields.map(({name,type,placeholder,icon: Icon,isPassword}) =>(
+          <div key={name} className={INPUTWRAPPER}>
+            <Icon className='text-purple-500 w-5 h-5'/>
+            <input type={type} placeholder={placeholder} value={formData[name]}onChange={(e) => setFormData({ ...formData,[next]: e.target.value})}
+            className='w-full focus:outline-none text-sm text-gray-700' required/>
+
+            {isPassword && (
+              <button type='button' onClick={() => setShowPassword((prev) => !prev)}
+              className='ml-2 text-gray-500 hover:text-purple-500 transition-colors'>
+              {showPassword ? <EyeOff className='w-5 h-5'/> : <Eye className='w-5 h-5'/>}  
+              </button>
+            )}
+          </div>
+        ))}
+      </form>
     </div>
   )
 }
