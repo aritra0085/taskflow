430b5c1d58169b61a66a1e5fcf002d7fbda86f14
aritra biswas <aritrabiswas0085@gmail.com>
2025-10-14 16:03:54 +0530

    backButton done in profile.jsx

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import {toast, ToastContainer} from 'react-toastify'
+import {BACK_BUTTON} from '../assets/dummy'
+import { ChevronLeft } from 'lucide-react'
 
 const Profile = () => {
   return (
-    <div>
-      
+    <div className='min-h-screen bg-gray-50'>
+      <ToastContainer position='top-center' autoClose={3000} />
+        <div className='max-w-4xl mx-auto p-6'>
+            <button onClick={() => NavigationHistoryEntry(-1)} className={BACK_BUTTON}> 
+                <ChevronLeft className='w-5 h-5 mr-1' />
+                Back to Dashboard
+            </button>
+
+        </div>
     </div>
   )
 }
