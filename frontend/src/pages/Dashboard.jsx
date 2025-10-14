673ed7eb095df4f5753f37cca0322dfcb7e2c7cf
aritra biswas <aritrabiswas0085@gmail.com>
2025-10-14 22:40:59 +0530

    new task button added

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ADD_BUTTON, HEADER, WRAPPER } from '../assets/dummy'
+import { HomeIcon, Plus } from 'lucide-react'
+
+const API_BASE = 'http://localhost:4000/api/tasks'
 
 const Dashboard = () => {
+
+  const [showModal, setShowModal] = useState(false)
+  const [selectedTask, setSelectedTask] = useState(null)
+  const [filter,setFilter] = useState('all')
+
   return (
-    <div>
-      d
+    <div className={WRAPPER}>
+        {/* HEADER */}
+        <div className={HEADER}>
+            <div className='min-w-0'>
+                <h1 className='text-xl md:text-3xl font-bold text-gray-800 flex items-center gap-2'>
+                  <HomeIcon className='text-purple-500 w-5 h-5 md:w-6 md:h-6 shrink-0'/>
+                  <span className='trancute'>Task Overview</span>
+                </h1>
+                <p className='text-sm text-gray-500 mt-1 ml-7 trancuate'>Manage your tasks efficiently</p>
+            </div>
+            <button onClick={() => setShowModal(true)} className={ADD_BUTTON}>
+              <Plus size={18} />
+              Add New Task
+            </button>
+        </div>
     </div>
   )
 }
