130190d21c1de2a7055fc7830dba26149574082f
aritra biswas <aritrabiswas0085@gmail.com>
2025-10-14 16:23:30 +0530

    logic use in back button and a little error fix

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {toast, ToastContainer} from 'react-toastify'
 import {BACK_BUTTON} from '../assets/dummy'
 import { ChevronLeft } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 
-const Profile = () => {
+const API_URL = 'http://localhost:4000'
+
+const Profile = ({setCurrentUser,onLogout}) => {
+
+    const [profile, setProfile] = useState ({name: '', email: ''})
+    const [passwords, setPasswords] = useState({current: '', new: '', confirm: ''})
+    const navigate = useNavigate();
+
   return (
     <div className='min-h-screen bg-gray-50'>
       <ToastContainer position='top-center' autoClose={3000} />
         <div className='max-w-4xl mx-auto p-6'>
-            <button onClick={() => NavigationHistoryEntry(-1)} className={BACK_BUTTON}> 
+            <button onClick={() => navigate(-1)} className={BACK_BUTTON}> 
                 <ChevronLeft className='w-5 h-5 mr-1' />
                 Back to Dashboard
             </button>
+
+            <div>
+
+            </div>
 
         </div>
     </div>
