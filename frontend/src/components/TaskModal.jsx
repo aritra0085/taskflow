e4c7bd5c0054cbd12702b47989073eb545389a63
aritra biswas <aritrabiswas0085@gmail.com>
2025-10-16 17:59:03 +0530

    taskModel continue

--- frontend/src/components/TaskModal.jsx
+++ frontend/src/components/TaskModal.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import { DEFAULT_TASK } from '../assets/dummy'
+import { PlusCircle, Save, X } from 'lucide-react'
 
-const TaskModal = () => {
+const API_BASE = 'http://localhost:4000/api/tasks'
+
+const TaskModal = ({isOpen, onClose, taskToEdit, onSave, onLogout}) => {
+
+    const [taskData, setTaskData] = useState(DEFAULT_TASK)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+    useEffect(() => {
+        if(!isOpen) return;
+        if(taskToEdit){
+            const normalized = taskToEdit.completed ==='yes' || taskToEdit.completed === true ? 'Yes' :  'No';
+            setTaskData({
+                ...DEFAULT_TASK,
+                title: taskToEdit.title || '',
+                description: taskToEdit.description || '',
+                priority: taskToEdit.priority || 'Low',
+                dueDate: taskToEdit.dueDate?.split('T')[0] || '',
+                completed: normalized,
+                id: taskToEdit._id,
+            });
+        }
+        else {
+            setTaskData(DEFAULT_TASK)
+        }
+        setError(null)
+    }, [isOpen, taskToEdit])
   return (
-    <div>
-      h
+    <div className='fixed inset-0 backdrop-blur-sm bg-black/20 z-50 flex items-center justify-center p-4'>
+      <div className='bg-white border border-purple-100 rounded-xl max-w-md w-full shadow-lg relative p-6 animate-fadeIn'>
+          <div className='flex justify-between items-center mb-6'>
+            <h2 className='text-2xl font-bold text-gray-800 flex items-center gap-2'>
+                {taskData.id ? <Save className='text-purple-500 w-5 h-5'/> :
+                <PlusCircle className=' text-purple-500 w-5 h-5'/>}
+                {taskData.id ? 'Edit Task' : 'Create New Task'}
+            </h2>
+
+            <button onClick={onClose} className='p-2 hover:bg-purple-100 rounded-lg transition-colors text-gray-500 hover:text-purple-700'>
+                    <X className='w-5 h-5'/>
+            </button>
+          </div>
+      </div>
     </div>
   )
 }
