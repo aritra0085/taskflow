1c68e595a45532f926921067419a45534185d43f
aritra biswas <aritrabiswas0085@gmail.com>
2025-10-19 11:24:33 +0530

    add js

--- frontend/src/components/TaskItem.jsx
+++ frontend/src/components/TaskItem.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TI_CLASSES } from '../assets/dummy'
 
 const API_BASE = 'http://localhost:4000/api/tasks'
 
 const TaskItem = ({task,onRefresh,onLogout,showCompleteCheckbox = true }) => {
+
+  const [showMenu,setShowMenu] = useState(false)
+  const [isCompleted, setIsCompleted] = useState(
+    [true, 1, 'yes'].includes(
+      typeof task.completed === 'string' ? task.completed.toLowerCase() : task.completed
+    )
+  )
+
   return (
        <>
-       
+         <div className={`${TI_CLASSES.wrapper} ${borderColor}`}>
+          
+          </div>       
        </>
   )
 }
