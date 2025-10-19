c9ecde39dc10fa98d8e3bb5c08f55f370392b839
aritra biswas <aritrabiswas0085@gmail.com>
2025-10-19 11:12:23 +0530

    starting with taskItem.jsx

--- frontend/src/components/TaskItem.jsx
+++ frontend/src/components/TaskItem.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const TaskItem = () => {
+const API_BASE = 'http://localhost:4000/api/tasks'
+
+const TaskItem = ({task,onRefresh,onLogout,showCompleteCheckbox = true }) => {
   return (
-    <div>
-      
-    </div>
+       <>
+       
+       </>
   )
 }
 
