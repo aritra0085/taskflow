8b170bd46bdfa9cb832f2a69c17d08416581e427
aritra biswas <aritrabiswas0085@gmail.com>
2025-10-21 17:29:50 +0530

    make the js for the completePage

--- frontend/src/pages/CompletePage.jsx
+++ frontend/src/pages/CompletePage.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
-
+import React, {useMemo, useState} from 'react'
+import { CT_CLASSES, SORT_OPTIONS } from '../assets/dummy'
+import { CheckCircle2 } from 'lucide-react'
+import { useOutletContext } from 'react-router-dom';
 const CompletePage = () => {
+
+  const {tasks, refreshTasks } = useOutletContext();
+  const [sortBy, setSortBy] = useState('newest')
+
+  const sortedCompletedTasks = useMemo(() => {
+      return tasks
+        .filter(task => [true, 1, 'yes'].includes(
+          typeof task.completed === 'string' ? task.completed.toLowerCase()
+          : task.completed
+        ))
+        .sort((a, b) => {
+          switch (sortBy) {
+            case 'newest':
+              return new Date(b.createAt) - new Date(a.createAt)
+              case 'oldest':
+              return new Date(a.createAt) - new Date(b.createAt)
+              case 'priority': {
+              const order = {high: 3, medium: 1, low: 1 };
+              return order[b.priority?.toLowerCase()] - order[a.priority?.toLowerCase()]
+              }
+              default:
+                return 0
+          }
+        })
+  }, [tasks, sortBy])
   return (
-    <div>
-      complete
+    <div className={CT_CLASSES.page}>
+      {/* HEADER */}
+      <div className={CT_CLASSES.header}>
+        <div className={CT_CLASSES.titleWrapper}>
+          <h1 className={CT_CLASSES.title}>
+            <CheckCircle2 className='text-purple-500 w-5 h-5 md:w-6 md:h-6'/>
+          </h1>
+
+          <p className={CT_CLASSES.subtitle}>
+            {}
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
