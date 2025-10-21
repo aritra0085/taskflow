a89f011fb1c8632b3b9c16e78d798ed99ec773d5
aritra biswas <aritrabiswas0085@gmail.com>
2025-10-21 12:13:04 +0530

    continue inPendingTask

--- frontend/src/pages/PendingPage.jsx
+++ frontend/src/pages/PendingPage.jsx
@@ -1,4 +1,4 @@
-import { Dice1, ListCheck } from 'lucide-react'
+import { ListCheck } from 'lucide-react'
 import React from 'react'
 import { layoutClasses } from '../assets/dummy'
 
@@ -10,6 +10,9 @@ const PendingPage = () => {
           <h1 className='text-2xl md:text-3xl font-bold text-gray-800 flex items-center gap-2'>
             <ListCheck className='text-purple-500'/> Pending Tasks
           </h1>
+          <p className='text-sm text-gray-500 mt-1 ml-7'>
+            
+          </p>
         </div>
       </div>
     </div>
