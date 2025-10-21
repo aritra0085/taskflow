9746fc20dec6f03969bb2c996c48aa571426a5fc
aritra biswas <aritrabiswas0085@gmail.com>
2025-10-21 12:11:13 +0530

    working pendingtask model

--- frontend/src/pages/PendingPage.jsx
+++ frontend/src/pages/PendingPage.jsx
@@ -1,9 +1,18 @@
-import { Dice1 } from 'lucide-react'
+import { Dice1, ListCheck } from 'lucide-react'
 import React from 'react'
+import { layoutClasses } from '../assets/dummy'
 
 const PendingPage = () => {
   return (
-    <Div></Div>
+    <div className={layoutClasses.container}>
+      <div className={layoutClasses.headerWrapper}>
+        <div>
+          <h1 className='text-2xl md:text-3xl font-bold text-gray-800 flex items-center gap-2'>
+            <ListCheck className='text-purple-500'/> Pending Tasks
+          </h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
